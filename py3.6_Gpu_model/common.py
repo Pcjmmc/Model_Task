2dff75869e93b1556905b9971bab58b73c8dc0a6
liuhuanling <liuhuanling@baidu.com>
2021-02-18 20:01:51 +0800

    更新模型task

---
+++ py3.6_Gpu_model/common.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+import os
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+HTTP_PROXY = "http://172.19.57.45:3128"
+HTTPS_PROXY = "http://172.19.57.45:3128"
+PADDLE_ON_MODEL_CE = "1"
+WITH_AVX = "ON"
+REPO_ADDR = "https://github.com/PaddlePaddle/models.git"
