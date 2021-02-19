b0bd86305c91933014b6cdbc0d875fb0ff86b84a
liuhuanling <liuhuanling@baidu.com>
2021-02-19 12:47:40 +0800

    更新yaml格式

--- py3.6_Gpu_model/common.py
+++ py3.6_Gpu_model/common.py
@@ -3,6 +3,5 @@ import os
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 HTTP_PROXY = "http://172.19.57.45:3128"
 HTTPS_PROXY = "http://172.19.57.45:3128"
-PADDLE_ON_MODEL_CE = "1"
 WITH_AVX = "ON"
 REPO_ADDR = "https://github.com/PaddlePaddle/models.git"
