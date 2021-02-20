0d220db1fa6d3825736ea5d16f25a9d5c05733b4
liuhuanling <liuhuanling@baidu.com>
2021-02-20 11:32:39 +0800

    配置restnet 50

--- py3.6_Gpu_model/common.py
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-import os
-ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-HTTP_PROXY = "http://172.19.57.45:3128"
-HTTPS_PROXY = "http://172.19.57.45:3128"
-WITH_AVX = "ON"
-REPO_ADDR = "https://github.com/PaddlePaddle/models.git"
---
+++ test_paddle_frame_with_model/common.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+import os
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+HTTP_PROXY = "http://172.19.57.45:3128"
+HTTPS_PROXY = "http://172.19.57.45:3128"
+
+NLP_ADDR = "https://github.com/PaddlePaddle/PaddleNLP.git"
+SEG_ADDR  = "https://github.com/PaddlePaddle/PaddleSeg.git"
+ClAS_ADDR = "https://github.com/PaddlePaddle/PaddleClas.git"
+DETECTION_ADDR = "https://github.com/PaddlePaddle/PaddleDetection.git"
