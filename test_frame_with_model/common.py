37ba1ec4d9f3551a5d068c77a2f7008c326b9dc9
liuhuanling <liuhuanling@baidu.com>
2021-02-20 20:32:34 +0800

    change proxy

--- test_frame_with_model/common.py
+++ test_frame_with_model/common.py
@@ -1,8 +1,8 @@
 # encoding: utf-8
 import os
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-HTTP_PROXY = "http://172.19.57.45:3128"
-HTTPS_PROXY = "http://172.19.57.45:3128"
+HTTP_PROXY = "http://172.19.61.250:3128"
+HTTPS_PROXY = "http://172.19.61.250:3128"
 
 NLP_ADDR = "https://github.com/PaddlePaddle/PaddleNLP.git"
 SEG_ADDR  = "https://github.com/PaddlePaddle/PaddleSeg.git"
