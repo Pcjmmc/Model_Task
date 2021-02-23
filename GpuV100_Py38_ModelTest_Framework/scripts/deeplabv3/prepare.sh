57b38169a3017c2109f99977e87a80984ea1da1d
liuhuanling <liuhuanling@baidu.com>
2021-02-23 16:50:03 +0800

    fix bug

--- GpuV100_Py38_ModelTest_Framework/scripts/deeplabv3/prepare.sh
+++ GpuV100_Py38_ModelTest_Framework/scripts/deeplabv3/prepare.sh
@@ -1,3 +1,5 @@
 export seg_dir=$PWD/../../seg_repo
 export data_path=$PWD/../../dataset
-ln -s ${dataset_path}/cityscape  ${seg_dir}/dataset/cityscapes
+# 数据存储路径
+mkdir -p ${seg_dir}/dataset/
+ln -s ${dataset_path}/cityscape  ${seg_dir}/dataset/cityscape
