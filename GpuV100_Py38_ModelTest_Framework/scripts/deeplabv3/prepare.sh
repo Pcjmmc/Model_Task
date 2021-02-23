f786f5f2736f6a5e9dc001c26f3a1c1ad9115f50
liuhuanling <liuhuanling@baidu.com>
2021-02-23 22:29:18 +0800

    调试deeplabv3

--- GpuV100_Py38_ModelTest_Framework/scripts/deeplabv3/prepare.sh
+++ GpuV100_Py38_ModelTest_Framework/scripts/deeplabv3/prepare.sh
@@ -1,6 +1,7 @@
-# export seg_dir=$PWD/../../seg_repo
+export seg_dir=$PWD/../../seg_repo
 # export data_path=$PWD/../../dataset
 # # 数据存储路径
 # mkdir -p ${seg_dir}/data/
 # ln -s ${dataset_path}/cityscape/ ${seg_dir}/data/cityscapes
+mkdir -p ${seg_dir}/data/
 ln -s /workspace/task/dataset/cityscape/ /workspace/task/seg_repo/data/cityscapes
