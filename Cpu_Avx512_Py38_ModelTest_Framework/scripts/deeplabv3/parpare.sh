cd3629909369a976f13b14afc5af0dded59fada2
liuhuanling <liuhuanling@baidu.com>
2021-02-23 20:00:43 +0800

    fix data path

--- Cpu_Avx512_Py38_ModelTest_Framework/scripts/deeplabv3/parpare.sh
+++ Cpu_Avx512_Py38_ModelTest_Framework/scripts/deeplabv3/parpare.sh
@@ -1,5 +1,5 @@
 export seg_dir=$PWD/../../seg_repo
 export data_path=$PWD/../../dataset
 # 数据存储路径
-mkdir -p ${seg_dir}/data/
-ln -s ${dataset_path}/cityscape/ ${seg_dir}/data/cityscapes
+mkdir -p ${seg_dir}/datasets/
+ln -s ${dataset_path}/cityscape/ ${seg_dir}/datasets/cityscapes
