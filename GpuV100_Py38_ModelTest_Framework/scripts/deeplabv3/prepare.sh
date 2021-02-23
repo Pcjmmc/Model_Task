0a61dd64bc23554d7b36f6b93e8f924c31b055d3
liuhuanling <liuhuanling@baidu.com>
2021-02-23 22:21:52 +0800

    写绝对路径

--- GpuV100_Py38_ModelTest_Framework/scripts/deeplabv3/prepare.sh
+++ GpuV100_Py38_ModelTest_Framework/scripts/deeplabv3/prepare.sh
@@ -1,5 +1,6 @@
-export seg_dir=$PWD/../../seg_repo
-export data_path=$PWD/../../dataset
-# 数据存储路径
-mkdir -p ${seg_dir}/data/
-ln -s ${dataset_path}/cityscape/ ${seg_dir}/data/cityscapes
+# export seg_dir=$PWD/../../seg_repo
+# export data_path=$PWD/../../dataset
+# # 数据存储路径
+# mkdir -p ${seg_dir}/data/
+# ln -s ${dataset_path}/cityscape/ ${seg_dir}/data/cityscapes
+ln -s /workspace/task/dataset/cityscape/ /workspace/task/seg_repo/data/cityscapes
