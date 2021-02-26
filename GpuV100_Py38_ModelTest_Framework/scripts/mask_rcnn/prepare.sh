3f73b5e3195cf9c2e42b6bebdd4a9e35f69e1352
liuhuanling <liuhuanling@baidu.com>
2021-02-26 13:59:33 +0800

    修改数据准备逻辑

--- GpuV100_Py38_ModelTest_Framework/scripts/mask_rcnn/prepare.sh
+++ GpuV100_Py38_ModelTest_Framework/scripts/mask_rcnn/prepare.sh
@@ -3,5 +3,6 @@ export seg_dir=$PWD/../../detection_repo
 # # 数据存储路径
 # mkdir -p ${seg_dir}/data/
 # ln -s ${dataset_path}/cityscape/ ${seg_dir}/data/cityscapes
+rm -rf ${seg_dir}/dataset/
 mkdir -p ${seg_dir}/dataset/
 ln -s /workspace/task/dataset/coco/ /workspace/task/detection_repo/dataset/coco
