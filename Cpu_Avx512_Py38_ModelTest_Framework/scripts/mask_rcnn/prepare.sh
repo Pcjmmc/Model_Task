d62e09ef59bfaae11bee70a1692d3528a0688020
liuhuanling <liuhuanling@baidu.com>
2021-02-26 15:31:03 +0800

    cpu新增模型

---
+++ Cpu_Avx512_Py38_ModelTest_Framework/scripts/mask_rcnn/prepare.sh
@@ -0,0 +1,8 @@
+export seg_dir=$PWD/../../detection_repo
+# export data_path=$PWD/../../dataset
+# # 数据存储路径
+# mkdir -p ${seg_dir}/data/
+# ln -s ${dataset_path}/cityscape/ ${seg_dir}/data/cityscapes
+rm -rf ${seg_dir}/dataset/
+mkdir -p ${seg_dir}/dataset/
+ln -s /workspace/task/dataset/coco/ /workspace/task/detection_repo/dataset/coco
