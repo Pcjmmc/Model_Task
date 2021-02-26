c1072e8a55e0c45e8df1a876d05a11e578c78d67
liuhuanling <liuhuanling@baidu.com>
2021-02-26 16:53:08 +0800

    增加日志转换步骤

--- GpuV100_Py38_ModelTest_Framework/common.py
+++ GpuV100_Py38_ModelTest_Framework/common.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 import os
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(ROOT_PATH)
 HTTP_PROXY = "http://172.19.61.250:3128"
 HTTPS_PROXY = "http://172.19.61.250:3128"
 
