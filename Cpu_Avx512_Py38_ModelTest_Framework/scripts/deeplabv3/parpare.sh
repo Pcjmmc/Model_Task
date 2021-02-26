64482c70f86a489529ce291d411e94a1ab669fbb
liuhuanling <liuhuanling@baidu.com>
2021-02-26 18:43:46 +0800

    修复数据路径

--- Cpu_Avx512_Py38_ModelTest_Framework/scripts/deeplabv3/parpare.sh
+++ Cpu_Avx512_Py38_ModelTest_Framework/scripts/deeplabv3/parpare.sh
@@ -1,3 +1,3 @@
 export seg_dir=$PWD/../../seg_repo
-mkdir -p ${seg_dir}/datasets/
-ln -s /workspace/task/dataset/cityscape/ /workspace/task/seg_repo/datasets/cityscapes
+mkdir -p ${seg_dir}/data/
+ln -s /workspace/task/dataset/cityscape/ /workspace/task/seg_repo/data/cityscapes
