d735983938bcfd8f062cf9af8405d9b64566b1f8
liuhuanling <liuhuanling@baidu.com>
2021-02-26 13:32:07 +0800

    修改数据路径

--- GpuV100_Py38_ModelTest_Framework/scripts/mask_rcnn/prepare.sh
+++ GpuV100_Py38_ModelTest_Framework/scripts/mask_rcnn/prepare.sh
@@ -3,5 +3,5 @@ export seg_dir=$PWD/../../detection_repo
 # # 数据存储路径
 # mkdir -p ${seg_dir}/data/
 # ln -s ${dataset_path}/cityscape/ ${seg_dir}/data/cityscapes
-mkdir -p ${seg_dir}/data/
-ln -s /workspace/task/dataset/coco/ /workspace/task/detection_repo/data/coco
+mkdir -p ${seg_dir}/dataset/
+ln -s /workspace/task/dataset/coco/ /workspace/task/detection_repo/dataset/coco
