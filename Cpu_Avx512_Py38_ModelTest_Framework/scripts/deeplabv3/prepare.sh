02c386677a597790943b0dcd5e1407fefd578de5
liuhuanling <liuhuanling@baidu.com>
2021-04-23 17:35:29 +0800

    日志统一存放

--- Cpu_Avx512_Py38_ModelTest_Framework/scripts/deeplabv3/prepare.sh
+++ Cpu_Avx512_Py38_ModelTest_Framework/scripts/deeplabv3/prepare.sh
@@ -1,3 +1,9 @@
 export seg_dir=$PWD/../../seg_repo
 mkdir -p ${seg_dir}/data/
 ln -s /workspace/task/dataset/cityscape/ /workspace/task/seg_repo/data/cityscapes
+
+# 创建日志路径
+cur_path=`pwd`
+model_name=${PWD##*/}
+log_path=$PWD/../../log/$model_name/
+mkdir -p $log_path
--- Cpu_Avx512_Py38_ModelTest_Framework/scripts/mask_rcnn/prepare.sh
+++ Cpu_Avx512_Py38_ModelTest_Framework/scripts/mask_rcnn/prepare.sh
@@ -6,3 +6,10 @@ export seg_dir=$PWD/../../detection_repo/static
 rm -rf ${seg_dir}/dataset/
 mkdir -p ${seg_dir}/dataset/
 ln -s /workspace/task/dataset/coco /workspace/task/detection_repo/static/dataset/coco
+
+
+# 创建日志路径
+cur_path=`pwd`
+model_name=${PWD##*/}
+log_path=$PWD/../../log/$model_name/
+mkdir -p $log_path
